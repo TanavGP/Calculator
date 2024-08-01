b2713b283c2a6bfd288a6115a0a4a90b91e5c1bd
TanavG <tanavg03@gmail.com>
2024-08-01 23:19:58 +0530

    basic operator functionality and simple expression evaluation

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+const DIV_BY_ZERO_MSG = "NOOOOOOOOOOOO!";
+const MOD_BY_ZERO_MSG = "WUUUUPSIIIEEE!";
+
+let precedence = {
+    "+" : 1,
+    "-" : 1,
+    "/" : 2,
+    "*" : 2,
+    "%" : 2
+};
+
+const expression = new Array();
+
+function isOperator(token) {
+    return (token === "+" || token === "-" || token === "*" || token === "/" || token === "%");
+}
+
+function isDigit(token) {
+    return !isNaN(token);
+}
+
 function createOperandButtons(leftContainerElement) {
     for (let i = 1; i <= 3; i++) {
         let rowElement = document.createElement('div');
@@ -34,8 +55,148 @@ function addButtonFunctionality() {
     buttonList.forEach(button => {
         button.addEventListener('click', (e) => {
             operate(e.target.textContent);
+            console.log("clicked a button!");
         })
     });
+}
+
+function updateResultDisplay() {
+    const resultDisplayElement = document.querySelector('#result-display');
+    resultDisplayElement.textContent = expression.toString().replace(/,/g, ' ');
+}
+
+function operate(token) {
+    token = token.toString().trim();
+    if (token === "C") {
+        expression.length = 0;
+        updateResultDisplay();
+    }
+    else if (token === "=") {
+        const result = evaluatePostfix(infixToPostfix(expression));
+        expression.length = 0;
+        expression.push(result);
+        updateResultDisplay();
+    }
+    else if (isOperator(token)) {
+        if (expression.length == 0) {
+            if (token === "-" || token === "+") {
+                expression.push(token);
+                updateResultDisplay();
+            }
+        }
+        else if (isOperator(expression[expression.length - 1])) {
+            expression.length -= 1;
+            expression.push(token);
+            updateResultDisplay();
+        }
+        else {    
+            expression.push(token);
+            updateResultDisplay();
+        }
+    }
+    else if (isDigit(token)) {
+        if (expression.length === 0 || isOperator(expression[expression.length - 1])) {
+            expression.push(token);
+            updateResultDisplay();
+        }
+        else {
+            expression[expression.length - 1] += token;
+            updateResultDisplay();
+        }
+    }
+    /*
+    // UNFINISHED
+    else if (token === "+/-") {
+        if (expression.length === 0) {
+            return;
+        } 
+        else if (expression[expression.length - 1] === "+") {
+            expression[expression.length - 1] = "-";
+            updateResultDisplay();
+        }
+        else if (expression[expression.length - 1] === "-") {
+            expression[expression.length - 1] = "+";
+            updateResultDisplay();
+        }
+        else if (isDigit(expression[expression.length - 1])) {
+            expression[expression.length - 1] = ""
+        }
+    }
+    */
+}
+
+function infixToPostfix(infix) {
+    const operator = new Array();
+    const postfix = new Array();
+
+    infix.forEach(token => {
+        if (isOperator(token)) {
+            while (operator.length > 0 && precedence[operator[operator.length - 1]] >= precedence[token]) {
+                postfix.push(operator[operator.length - 1]);
+                operator.pop();
+            }
+            operator.push(token);
+        }
+        else {
+            postfix.push(token);
+        }
+    });
+    while(operator.length > 0) {
+        postfix.push(operator[operator.length - 1]);
+        operator.pop();
+    }
+
+    return postfix;
+}
+
+function evaluatePostfix(postfix) {
+    const tempStack = new Array();
+    let errorMSG = "";
+    let errorFlag = false;
+    postfix.forEach(token => {
+        if (!errorFlag && isOperator(token)) {
+            const num2 = BigInt(tempStack[tempStack.length - 1]);
+            tempStack.pop();
+            const num1 = BigInt(tempStack[tempStack.length - 1]);
+            tempStack.pop();
+
+            let result = BigInt(-1);
+            switch(token) {
+                case "+" : result = num1 + num2; break;
+                case "-" : result = num1 - num2; break;
+                case "*" : result = num1 * num2; break;
+                case "/" : 
+                    if (num2 === BigInt(0)) {
+                        errorMSG = DIV_BY_ZERO_MSG;
+                        errorFlag = true;
+                    }  
+                    else {
+                        result = num1 / num2;
+                    }
+                    break;
+                case "%" : 
+                    if (num2 === BigInt(0)) {
+                        errorMSG = MOD_BY_ZERO_MSG;
+                        errorFlag = true;
+                    }
+                    else {
+                        result = num1 % num2;
+                    }
+                    break;
+            }
+            if (!errorFlag) {
+                tempStack.push(result.toString());
+            }
+        }
+        else {
+            tempStack.push(token);
+        }
+    })
+    
+    if (errorFlag) {
+        return errorMSG;
+    }
+    return tempStack.length === 0 ? "EMPTY" : tempStack[0];
 }
 
 const leftContainerElement = document.querySelector('#left-container');
